18ffa868cc61d3977c07783cc0236db541888a0a
Hoac Phong <hphong@allivet.com>
2024-05-29 23:16:23 +0700

    about section

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,24 @@ const navMenu = document.querySelector("#nav_menu");
 navTriggerBtn.addEventListener("click", () => {
     navMenu.classList.toggle("nav-is-open");
 })
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2
+        },
+        960: {
+            slidesPerView: 3
+        }
+    }
+})
